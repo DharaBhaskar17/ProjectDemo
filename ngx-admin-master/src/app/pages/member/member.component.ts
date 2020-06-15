b6db2e6c0dc85ddc2f5e58d0f57336c8e4cd0b10
DharaBhaskar17 <dhara13bhaskar@gmail.com>
2020-06-15 14:58:05 +0530

    table add on  member module

--- ngx-admin-master/src/app/pages/member/member.component.ts
+++ ngx-admin-master/src/app/pages/member/member.component.ts
@@ -8,21 +8,28 @@ import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 })
 export class MemberComponent implements OnInit {
 
-  public memberForm: FormGroup;
+  memberData: any;
+  memberForm: FormGroup;
   submitted = false;
+
 
   constructor(private formbulider: FormBuilder) {
     const memberItems = [];
-    const phoneNoPattern = '^[0-9]{10}$';
-    const memberNamePattern = '[a-zA-Z]+';
     memberItems.push(this.formbulider.group({
-      MemberName: ['', [Validators.required, Validators.pattern(memberNamePattern)]],
-      MemberPhoneNo: ['', [Validators.required, Validators.pattern(phoneNoPattern), Validators.minLength(10)]]
+      MemberName: [],
+      MemberPhoneNo: []
     }));
 
-    this.memberForm = this.formbulider.group({
-      memberDetails: this.formbulider.array(memberItems)
-    })
+    // const phoneNoPattern = '^[0-9]{10}$';
+    // const memberNamePattern = '[a-zA-Z]+';
+    // memberItems.push(this.formbulider.group({
+    //   MemberName: ['', [Validators.required, Validators.pattern(memberNamePattern)]],
+    //   MemberPhoneNo: ['', [Validators.required, Validators.pattern(phoneNoPattern), Validators.minLength(10)]]
+    // }));
+
+    // this.memberForm = this.formbulider.group({
+    //   memberDetails: this.formbulider.array(memberItems)
+    // })
   }
 
   ngOnInit(): void {
@@ -43,20 +50,70 @@ export class MemberComponent implements OnInit {
   }
 
   CreateNewMember() {
-    const phoneNoPattern = '^[0-9]{10}$';
-    const memberNamePattern = '[a-zA-Z]+';
     return this.formbulider.group({
-      MemberName: ['', [Validators.required, Validators.pattern(memberNamePattern)]],
-      MemberPhoneNo: ['', [Validators.required, Validators.pattern(phoneNoPattern), Validators.maxLength(10)]]
+      MemberName: [],
+      MemberPhoneNo: []
     })
-    
+
+    // const phoneNoPattern = '^[0-9]{10}$';
+    // const memberNamePattern = '[a-zA-Z]+';
+    // return this.formbulider.group({
+    //   MemberName: ['', [Validators.required, Validators.pattern(memberNamePattern)]],
+    //   MemberPhoneNo: ['', [Validators.required, Validators.pattern(phoneNoPattern), Validators.maxLength(10)]]
+    // })
+
   }
 
 
   SaveMember() {
+    const memberItems = [];
+    const memberDetail = this.memberForm.get('memberDetails') as FormArray;
+    const phoneNoPattern = '^[0-9]{10}$';
+    const memberNamePattern = '[a-zA-Z]+';
+
+    for (let index = 0; index < memberDetail.controls.length; index++) {
+      const element = memberDetail.controls[index];
+      let item = element['controls'];
+      if (!item.MemberName.value || !item.MemberPhoneNo.value) {
+        let memberNameValue = item.MemberName;
+        let memberPhoneNoValue = item.MemberPhoneNo;
+
+        memberItems.push(this.formbulider.group({
+          memberNameValue: ['', [Validators.required, Validators.pattern(memberNamePattern)]],
+          memberPhoneNoValue: ['', [Validators.required, Validators.pattern(phoneNoPattern), Validators.minLength(10)]]
+        }))
+      }
+      this.memberForm = this.formbulider.group({
+        memberDetails: this.formbulider.array(memberItems)
+      })
+    }
+
+    // this.memberForm = this.formbulider.group({
+    //   memberDetails: this.formbulider.array(memberItems)
+    // })
+    // const memberItems = [];
+    // const phoneNoPattern1 = '^[0-9]{10}$';
+    // const memberNamePattern1= '[a-zA-Z]+';
+    // memberItems.push(this.formbulider.group({
+    //   MemberName: ['', [Validators.required, Validators.pattern(memberNamePattern1)]],
+    //   MemberPhoneNo: ['', [Validators.required, Validators.pattern(phoneNoPattern1), Validators.minLength(10)]]
+    // }));
+
+    // this.memberForm = this.formbulider.group({
+    //   memberDetails: this.formbulider.array(memberItems)
+    // })
+
     this.submitted = true;
     if (this.memberForm.invalid) {
       return;
+    }
+    else {
+      let item = {};
+      item = {
+        MemberName: this.memberForm.controls.MemberName.value,
+        MemberPhoneNo: this.memberForm.controls.MemberPhoneNo.value,
+      }
+      this.memberData.push(item);
     }
   }
 }
